a7f8dbdb9fa3124ba5d3307140c2a3785c95669e
Rafał Kot <rafalkot@gmail.com>
2014-12-18 11:52:58 +0100

    First output version

--- import.js
+++ import.js
@@ -1,8 +1,37 @@
 #!/usr/bin/env node
 
 var FB = require('fb');
+var async = require('async'); 
+var moment = require('moment');
+
+var posts = []; 
+
 FB.setAccessToken(process.env.FB_ACCESSTOKEN);
 
 FB.api(process.env.FB_SITE, function(res) {
-	console.log(res);
+	var i = 1;
+	async.forEach(res.data, function (item, callback){ 
+	    console.log(item);
+	    if(!item.message) {
+		   return callback();
+	    }
+	    var post = {
+		"id": item.id.toString().split("_")[1],
+		"title": "Post",
+		"html": item.message,
+		"image": null,
+		"featured": 0,
+		"page": 0,
+		"author_id":1,
+		"created_at": moment(item.created_time).unix()
+	    };
+	    if(item.picture) {
+		    post.image = item.picture;
+	    }
+	    posts.push(post);
+	    callback();
+	}, function(err) {
+	    console.log(posts);
+	}); 
 });
+
